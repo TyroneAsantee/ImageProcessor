6165db8098b1ea457ef216ffc4f485b56dffa3db
Tyrone Michael Asante <tyrone@MacBook-Air-som-tillhor-Tyrone.local>
2025-10-07 17:42:14 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module kth.se.tyronea.hi1027labb3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens kth.se.tyronea.hi1027labb3 to javafx.fxml;
+    exports kth.se.tyronea.hi1027labb3;
+}
